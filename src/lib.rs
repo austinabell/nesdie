7dfeb2aae5c6af8a5588a76dfad6bead814bfe3f
austinabell <austinabell8@gmail.com>
2021-06-26 19:33:23 -0400

    setup wee_alloc in wasm32

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod sys;
+
+// Set up global allocator by default if wee_alloc feature is enabled and in wasm32 architecture.
+#[cfg(all(feature = "wee_alloc", target_arch = "wasm32"))]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
