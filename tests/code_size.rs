15e2b91bf5ebdc1e5b7a0835c7f381e3f182b03d
austinabell <austinabell8@gmail.com>
2021-07-19 15:32:01 -0400

    add code size tests for some examples

---
+++ tests/code_size.rs
@@ -0,0 +1,54 @@
+/// Compiles contract to wasm with release configuration and returns the code size.
+fn check_example_size(example: &str) -> usize {
+    let status = std::process::Command::new("cargo")
+        .env("RUSTFLAGS", "-C link-arg=-s")
+        .args(&[
+            "build",
+            "--release",
+            "--target",
+            "wasm32-unknown-unknown",
+            "--manifest-path",
+        ])
+        .arg(format!("./examples/{}/Cargo.toml", example))
+        .status()
+        .unwrap();
+    if !status.success() {
+        panic!("building wasm example returned non-zero code {}", status);
+    }
+
+    let wasm = std::fs::read(format!(
+        "./examples/{}/target/wasm32-unknown-unknown/release/{}.wasm",
+        example,
+        example.replace("-", "_")
+    ))
+    .unwrap();
+
+    wasm.len()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn proxy_code_size_check() {
+    let size = check_example_size("proxy");
+
+    // 3078
+    assert!(size < 3200);
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn raw_contract_code_size_check() {
+    let size = check_example_size("raw-contract");
+
+    // 169
+    assert!(size < 250);
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn fungible_token_code_size_check() {
+    let size = check_example_size("smol_ft");
+
+    // 1270
+    assert!(size < 1350);
+}
