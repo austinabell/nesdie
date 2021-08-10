2103958a645efed57d4db58eb52db93c3fc81d11
austinabell <austinabell8@gmail.com>
2021-08-10 19:54:08 -0400

    drop dead code

--- src/lib.rs
+++ src/lib.rs
@@ -51,31 +51,3 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
         unsafe { core::arch::wasm32::unreachable() }
     }
 }
-
-mod lib {
-    mod core {
-        pub use core::*;
-    }
-
-    pub use self::core::cell::{Cell, RefCell};
-    pub use self::core::clone::{self, Clone};
-    pub use self::core::convert::{self, From, Into};
-    pub use self::core::default::{self, Default};
-    pub use self::core::fmt::{self, Debug, Display};
-    pub use self::core::hash::{self, Hash};
-    pub use self::core::iter::FusedIterator;
-    pub use self::core::marker::{self, PhantomData};
-    pub use self::core::ops::{Bound, RangeBounds};
-    pub use self::core::result::{self, Result};
-    pub use self::core::{borrow, char, cmp, iter, mem, num, ops, slice, str};
-
-    // pub use alloc::borrow::{Cow, ToOwned};
-
-    // pub use alloc::string::{String, ToString};
-
-    // pub use alloc::vec::{self, Vec};
-
-    // pub use alloc::boxed::Box;
-
-    // pub use alloc::collections::{btree_map, BTreeMap};
-}
